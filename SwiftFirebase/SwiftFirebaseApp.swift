65eb2e250c92d797b9c2fbd94aa1629d917f49de
Anlprnc <anilpirincciii@gmail.com>
2024-11-13 15:11:26 +0300

    Initial Commit

---
+++ SwiftFirebase/SwiftFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftFirebaseApp.swift
+//  SwiftFirebase
+//
+//  Created by Anıl on 13.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
